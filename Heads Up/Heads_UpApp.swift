cb2ae453f5fc56ac991f82ff127574c3abfd47ed
İlke Saykı <ilkesayki@hotmail.com>
2026-01-25 18:10:52 +0100

    Initial Commit

---
+++ Heads Up/Heads_UpApp.swift
@@ -0,0 +1,17 @@
+//
+//  Heads_UpApp.swift
+//  Heads Up
+//
+//  Created by İlke Saykı on 25.01.26.
+//
+
+import SwiftUI
+
+@main
+struct Heads_UpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
